6dbdc71d412f78edbc5d647d9f6a24977d3d892d
ian <onlordev@gmail.com>
2022-12-13 13:51:47 +0900

    ref file

--- my-app/src/pages/home/Home.jsx
+++ my-app/src/pages/home/Home.jsx
@@ -1,9 +1,12 @@
+import homeImg from "../../images/homeImg.jpg";
+
 const Home = () => {
   const token = window.localStorage.getItem("JWT");
   if (token) window.location.replace("/todos");
   return (
-    <article className="flex justify-center items-center h-96 text-xl">
-      <p>Let's start the 'To-Do List' !</p>
+    <article className="flex flex-col justify-center items-center text-xl mt-10">
+      <p className="my-5">Let's start the 'To-Do List' !</p>
+      <img src={homeImg} alt="home" className="w-3/5" />
     </article>
   );
 };
